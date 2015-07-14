6242ad70aa851768cc53bdb940e102eaa268477b
George Kraft <cynerva@gmail.com>
2015-07-13 21:53:50 -0500

    Draw lines! liiiines!

--- src/theater/core.clj
+++ src/theater/core.clj
@@ -6,23 +6,36 @@
   (q/frame-rate 60)
   (q/background 128))
 
-(defn clear []
+(defn fill-screen [& colors]
   (q/stroke-weight 0)
-  (q/fill 0)
+  (apply q/fill colors)
   (q/rect 0 0 (q/width) (q/height)))
 
-(defn draw []
-  (q/stroke (q/random 255) (q/random 255) (q/random 255))
-  (q/stroke-weight (q/random 10))
-  (q/fill 0)
-  (doseq [i (range 10)]
-    (let [d (q/random 100)]
-      (q/ellipse (q/random (q/width))
-                 (q/random (q/height))
-                 d d))))
+(defn draw-random-horizontal-line []
+  (let [y (q/random (q/height))]
+    (q/line 0 y (q/width) y)))
 
-(q/defsketch example
-  :title "Oh so many grey circles"
-  :setup setup
-  :draw draw
-  :size [1366 768])
+(defn draw-random-vertical-line []
+  (let [x (q/random (q/width))]
+    (q/line x 0 x (q/height))))
+
+(defn draw-random-line []
+  (if (< (q/random) 0.5)
+    (draw-random-horizontal-line)
+    (draw-random-vertical-line)))
+
+(defn draw-random-lines [count]
+  (doseq [_ (range count)]
+    (draw-random-line)))
+
+(defn draw []
+  ;(q/random-seed 0)
+  ;(fill-screen 0)
+  (q/stroke 255)
+  (q/stroke-weight 10)
+  (draw-random-line))
+
+(q/sketch
+ :setup setup
+ :draw draw
+ :size [1366 768])
