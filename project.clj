ca5f5e39b41938edd42d94c2bbbabfe9502f9842
George Kraft <cynerva@gmail.com>
2015-07-19 14:48:07 -0500

    Add ddf.minim dependency

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject theater "0.0.0"
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [quil "2.2.6"]])
+                 [quil "2.2.6"]
+                 [ddf.minim "2.2.0"]])
